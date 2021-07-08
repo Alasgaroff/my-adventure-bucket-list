289a06075a36fba2269f5f3321682316293a2975
Khayal <alaskarov.khayal@gmail.com>
2021-07-08 01:41:41 -0700

    did some changes on css

--- client/components/Title.js
+++ client/components/Title.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react'
 class Title extends Component {
     render(){
         return <div>
-                <h1>My Adventure Portfolio</h1>
+                <h1>My Adventure Bucket </h1>
+                <h2>Take Vacations </h2>
+                <h2>go as many places as you can</h2>
+                <h2>You can always make money, but you can't always make memories!</h2>
                 <h3>Beautiful things happen in your life when you distance yourself from negativity!</h3>
                </div>
     }
